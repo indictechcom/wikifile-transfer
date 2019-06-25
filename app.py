38ad6cb4c0ecedc3fcad6758a572166906dd247a
Jayprakash12345 <0freerunning@gmail.com>
2019-06-26 04:06:30 +0530

    Added OAuth and make UI better

--- app.py
+++ app.py
@@ -1,19 +1,38 @@
 from flask import Flask, render_template
-# import requests, json
+from flask_mwoauth import MWOAuth
+import requests_oauthlib
+import requests
+import os
+import yaml
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
+# Load configuration from YAML file
+__dir__ = os.path.dirname(__file__)
+app.config.update(yaml.safe_load(open(os.path.join(__dir__, 'config.yaml'))))
+
+# Get variables
+BASE_URL = app.config['OAUTH_MWURI']
+API_ENDPOINT = BASE_URL + '/api.php'
+CONSUMER_KEY = app.config['CONSUMER_KEY']
+CONSUMER_SECRET = app.config['CONSUMER_SECRET']
+
+# Register blueprint to app
+MWOAUTH = MWOAuth(base_url=BASE_URL, consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET)
+app.register_blueprint(MWOAUTH.bp)
+
+
+# /index route for return_to
+@app.route('/index', methods=['GET'])
 @app.route("/")
 def index():
-    data = {'username': 'rammanoj', 'auth': True}
+
+    data = {
+        'username': MWOAUTH.get_current_user(True)
+    }
     return render_template('index.html', data=data)
 
 
-@app.route("/home")
-def home():
-    return render_template('home.html')
-
-
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='8080', debug=True)
+    app.run()
