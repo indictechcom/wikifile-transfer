16751f009fc975486273af8e160aaff116c8a944
Riya Verma <ryaverma28@gmail.com>
2026-06-22 15:04:10 -0700

    fix: increase language column size from String(4) to String(20) (closes #78) (#79)

--- model.py
+++ model.py
@@ -6,8 +6,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255))
     pref_project = db.Column(db.String(15))
-    pref_language = db.Column(db.String(4))
-    user_language = db.Column(db.String(4), default='en')
+    pref_language = db.Column(db.String(20))
+    user_language = db.Column(db.String(20), default='en')
     skip_upload_selection = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
